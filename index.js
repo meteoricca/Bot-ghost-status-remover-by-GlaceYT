284cea8071fda691e5a8398beb7102bdae9d54e0
meteoricca <142053973+meteoricca@users.noreply.github.com>
2024-07-25 13:30:51 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Playing Cherry by Lana Del Rey","Playing Cherry by Lana Del Rey"];
 
 
 let currentIndex = 0;
